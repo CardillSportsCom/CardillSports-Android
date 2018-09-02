3d459ecfed068e52ef5a8fe38ee5e0da10966492
Vithushan <vithushan19@gmail.com>
2018-09-02 08:55:06 -0400

    try using table view

--- app/src/main/java/com/cardillsports/stattracker/game/ui/DetailsActivity.java
+++ app/src/main/java/com/cardillsports/stattracker/game/ui/DetailsActivity.java
@@ -6,13 +6,23 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.cardillsports.stattracker.R;
+import com.cardillsports.stattracker.details.businesslogic.StatsTableAdapter;
 import com.cardillsports.stattracker.game.businesslogic.GamePlayerAdapter;
 import com.cardillsports.stattracker.game.data.GameData;
 import com.cardillsports.stattracker.game.data.GameRepository;
+import com.cardillsports.stattracker.game.data.Stat;
+import com.cardillsports.stattracker.game.data.StatType;
+import com.evrencoskun.tableview.TableView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjection;
+
+import static java.security.AccessController.getContext;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -27,17 +37,20 @@ public class DetailsActivity extends AppCompatActivity {
 
         GameData gameData = gameRepository.getGameStats();
 
-        if (gameData != null) {
-            RecyclerView teamOneRecyclerView = findViewById(R.id.team_1_recycler_view);
-            teamOneRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-            GamePlayerAdapter teamOneAdapter = new GamePlayerAdapter(gameData.teamOnePlayers(), gameRepository);
-            teamOneRecyclerView.setAdapter(teamOneAdapter);
+        TableView tableView =findViewById(R.id.team_1_table_view);
 
-            RecyclerView teamTwoRecyclerView = findViewById(R.id.team_2_recycler_view);
-            teamTwoRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-            GamePlayerAdapter teamTwoAdapter = new GamePlayerAdapter(gameData.teamTwoPlayers(), gameRepository);
-            teamTwoRecyclerView.setAdapter(teamTwoAdapter);
-        }
+        // Create our custom TableView Adapter
+        StatsTableAdapter adapter = new StatsTableAdapter(this);
+
+        // Set this adapter to the our TableView
+        tableView.setAdapter(adapter);
+
+        // Let's set datas of the TableView on the Adapter
+        List<StatType> columnHeaderItems = Arrays.asList(StatType.values());
+
+        List<List<Stat>> mCellList = new ArrayList<>();
+
+        adapter.setAllItems(columnHeaderItems, gameData.teamOnePlayers(), mCellList);
     }
 }
 
