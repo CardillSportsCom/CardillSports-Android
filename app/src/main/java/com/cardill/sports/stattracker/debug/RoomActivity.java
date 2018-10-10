0a0c63b9e9e7e894248faf265fb3ff5846e04ef6
Vithushan Namasivayasivam <vithushan19@gmail.com>
2018-10-10 09:12:42 -0400

    write to external storage logs

--- app/src/main/java/com/cardill/sports/stattracker/debug/RoomActivity.java
+++ app/src/main/java/com/cardill/sports/stattracker/debug/RoomActivity.java
@@ -1,6 +1,11 @@
 package com.cardill.sports.stattracker.debug;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -9,6 +14,12 @@ import com.cardill.sports.stattracker.game.data.GameData;
 import com.cardill.sports.stattracker.network.MockData;
 import com.cardill.sports.stattracker.offline.domain.LocalGameRepository;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
@@ -20,10 +31,12 @@ import timber.log.Timber;
 
 public class RoomActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_WRITE_STORAGE = 100;
     @Inject
     LocalGameRepository gameDataStore;
 
     private FirebaseAnalytics mFirebaseAnalytics;
+    private ArrayList<GameData> gameData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,14 +64,81 @@ public class RoomActivity extends AppCompatActivity {
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(gameData ->
                     {
-                        mFirebaseAnalytics.setUserId("VITHUSHAN'S PHONE");
-                        Bundle params = new Bundle();
-                        params.putParcelableArrayList("game-recovery-key", gameData);
-                        mFirebaseAnalytics.logEvent("game_recovery", params);
+                        if (ContextCompat.checkSelfPermission(this,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                                && isExternalStorageWritable()) {
+                            this.gameData = gameData;
+                            writeToDownloads(this.gameData);
+                        } else {
+                            ActivityCompat.requestPermissions(this,
+                                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                    PERMISSION_WRITE_STORAGE);
+                        }
                         Timber.tag("VITHUSHAN").d("JUST GOT: %s", gameData.size());
                     });
 
         });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_WRITE_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    writeToDownloads(gameData);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    private void writeToDownloads(ArrayList<GameData> gameData) {
+        Gson gson = new Gson();
+        String s = gson.toJson(gameData);
+        FileOutputStream outputStream;
+
+        try {
+            File storageDir = getPublicAlbumStorageDir("cardill");
+            File file = new File(storageDir, "logs.txt");
+            outputStream = new FileOutputStream(file);
+            outputStream.write(s.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public File getPublicAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), albumName);
+
+        if (!file.mkdirs()) {
+            Timber.e("Directory not created");
+        }
+
+        return file;
 
     }
 
