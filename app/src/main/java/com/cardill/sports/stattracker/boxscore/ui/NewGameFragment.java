fbcd9469110a8416441f4cdec28a1f4ff5f7f186
Vithushan Namasivayasivam <vithushan19@gmail.com>
2018-10-13 14:50:18 -0400

    finish ads

--- app/src/main/java/com/cardill/sports/stattracker/boxscore/ui/NewGameFragment.java
+++ app/src/main/java/com/cardill/sports/stattracker/boxscore/ui/NewGameFragment.java
@@ -19,6 +19,7 @@ import com.cardill.sports.stattracker.ui.MainActivity;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.formats.NativeAdOptions;
 import com.google.android.gms.ads.formats.NativeAppInstallAd;
 import com.google.android.gms.ads.formats.NativeContentAd;
@@ -40,21 +41,8 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class NewGameFragment extends Fragment {
 
-    // The number of native ads to load.
-    public static final int NUMBER_OF_ADS = 5;
-
-    // The AdLoader used to load ads.
-    private AdLoader adLoader;
-
-    // List of MenuItems and native ads that populate the RecyclerView.
-    private List<Object> mRecyclerViewItems = new ArrayList<>();
-
-    // List of native ads that have been successfully loaded.
-    private List<UnifiedNativeAd> mNativeAds = new ArrayList<>();
-
     private View view;
-    private ProgressBar progress;
-    private RecyclerView recyclerView;
+    private AdView mAdView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -77,10 +65,7 @@ public class NewGameFragment extends Fragment {
                 x -> NavHostFragment.findNavController(this)
                         .navigate(R.id.action_newGameFragment_to_teamCreationActivity));
 
-        recyclerView = view.findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
-        progress = view.findViewById(R.id.progress);
+        mAdView = view.findViewById(R.id.adView);
 
         return view;
     }
@@ -88,124 +73,10 @@ public class NewGameFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        loadAds();
-    }
-
-    private void loadAds() {
-        // Update the RecyclerView item's list with menu items.
-        addMenuItemsFromJson();
-        // Update the RecyclerView item's list with native ads.
-        loadNativeAds();
-    }
-
-    /**
-     * Adds {@link MenuItem}'s from a JSON file.
-     */
-    private void addMenuItemsFromJson() {
-        try {
-            String jsonDataString = readJsonDataFromFile();
-            JSONArray menuItemsJsonArray = new JSONArray(jsonDataString);
-
-            for (int i = 0; i < menuItemsJsonArray.length(); ++i) {
-
-                JSONObject menuItemObject = menuItemsJsonArray.getJSONObject(i);
-
-                String menuItemName = menuItemObject.getString("name");
-                String menuItemDescription = menuItemObject.getString("description");
-                String menuItemPrice = menuItemObject.getString("price");
-                String menuItemCategory = menuItemObject.getString("category");
-                String menuItemImageName = menuItemObject.getString("photo");
-
-                MenuItem menuItem = new MenuItem(menuItemName, menuItemDescription, menuItemPrice,
-                        menuItemCategory, menuItemImageName);
-                //mRecyclerViewItems.add(menuItem);
-            }
-        } catch (IOException | JSONException exception) {
-            Log.e(MainActivity.class.getName(), "Unable to parse JSON file.", exception);
-        }
-    }
-
-    /**
-     * Reads the JSON file and converts the JSON data to a {@link String}.
-     *
-     * @return A {@link String} representation of the JSON data.
-     * @throws IOException if unable to read the JSON file.
-     */
-    private String readJsonDataFromFile() throws IOException {
-
-        InputStream inputStream = null;
-        StringBuilder builder = new StringBuilder();
-
-        try {
-            String jsonDataString = null;
-            inputStream = getResources().openRawResource(R.raw.menu_items_json);
-            BufferedReader bufferedReader = new BufferedReader(
-                    new InputStreamReader(inputStream, "UTF-8"));
-            while ((jsonDataString = bufferedReader.readLine()) != null) {
-                builder.append(jsonDataString);
-            }
-        } finally {
-            if (inputStream != null) {
-                inputStream.close();
-            }
-        }
-
-        return new String(builder);
-    }
-
-    private void loadNativeAds() {
-
-        AdLoader.Builder builder = new AdLoader.Builder(getContext(), getString(R.string.ad_unit_id));
-        adLoader = builder.forUnifiedNativeAd(
-                new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
-                    @Override
-                    public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
-                        // A native ad loaded successfully, check if the ad loader has finished loading
-                        // and if so, insert the ads into the list.
-                        mNativeAds.add(unifiedNativeAd);
-                        if (!adLoader.isLoading()) {
-                            insertAdsInMenuItems();
-                        }
-                    }
-                }).withAdListener(
-                new AdListener() {
-                    @Override
-                    public void onAdFailedToLoad(int errorCode) {
-                        // A native ad failed to load, check if the ad loader has finished loading
-                        // and if so, insert the ads into the list.
-                        Log.e("TAG", errorCode + "");
-                        Log.e("MainActivity", "The previous native ad failed to load. Attempting to"
-                                + " load another.");
-                        if (!adLoader.isLoading()) {
-                            insertAdsInMenuItems();
-                        }
-                    }
-                }).build();
-
-        // Load the Native ads.
-        adLoader.loadAds(new AdRequest.Builder()
-                .addKeyword("nba")
+        AdRequest adRequest = new AdRequest.Builder()
                 .addKeyword("basketball")
-                .build(), NUMBER_OF_ADS);
-    }
+                .build();
+        mAdView.loadAd(adRequest);
 
-    private void insertAdsInMenuItems() {
-        if (mNativeAds.size() <= 0) {
-            return;
-        }
-
-        int offset = (mRecyclerViewItems.size() / mNativeAds.size()) + 1;
-        int index = 0;
-        for (UnifiedNativeAd ad : mNativeAds) {
-            mRecyclerViewItems.add(index, ad);
-            index = index + offset;
-        }
-
-        renderAds();
-    }
-
-    private void renderAds() {
-        recyclerView.setAdapter(new RecyclerViewAdapter(getContext(), mRecyclerViewItems));
-        progress.setVisibility(View.GONE);
     }
 }
