ab1acc6dbc03e4d18e09165d88485c00367162de
Vithushan Namasivayasivam <vithushan19@gmail.com>
2018-11-27 22:15:46 -0500

    rename auth interceptor variables

--- app/src/main/java/com/cardill/sports/stattracker/user/UnauthorizedInterceptor.java
+++ app/src/main/java/com/cardill/sports/stattracker/user/UnauthorizedInterceptor.java
@@ -1,20 +1,15 @@
 package com.cardill.sports.stattracker.user;
 
 import com.cardill.sports.stattracker.AuthService;
-import com.cardill.sports.stattracker.network.CardillService;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.io.IOException;
 
-import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
 import timber.log.Timber;
 
 /**
@@ -23,11 +18,11 @@ import timber.log.Timber;
 
 public class UnauthorizedInterceptor implements Interceptor {
     private Session session;
-    private AuthService cardillService;
+    private AuthService authService;
 
     public UnauthorizedInterceptor(Session session, AuthService cardillService) {
         this.session = session;
-        this.cardillService = cardillService;
+        this.authService = cardillService;
     }
 
     @Override
@@ -36,14 +31,14 @@ public class UnauthorizedInterceptor implements Interceptor {
         Request mainRequest = chain.request();
 
         if (mainResponse.code() == 401 || mainResponse.code() == 403) {
-            //session.invalidate();
+            session.invalidate();
 
             FirebaseAuth.getInstance()
                     .getCurrentUser()
                     .getIdToken(true)
                     .addOnCompleteListener(task -> {
                         String tokenString = task.getResult().getToken();
-                        cardillService.authenticate(new AuthRequestBody(tokenString))
+                        authService.authenticate(new AuthRequestBody(tokenString))
                                 .map(authResponseBody -> {
                                     Request.Builder builder = mainRequest.newBuilder()
                                             .header("Authorization", authResponseBody.getId_token())
